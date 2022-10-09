392e43c83c48442f74ebcac1d77e139a7f120caa
OnyxQueen0128 <107898155+OnyxQueen0128@users.noreply.github.com>
2022-10-09 20:17:36 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <div className="container"></div>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/lysandrawj/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lysandra Webba Jorge
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/OnyxQueen0128/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
