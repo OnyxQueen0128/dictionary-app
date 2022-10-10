c77a8c792b4fad1a4e49cba29f7914aa6fec21de
OnyxQueen0128 <107898155+OnyxQueen0128@users.noreply.github.com>
2022-10-10 16:02:34 +0200

    API integrated

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function SearchResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function Search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    //API documentation from: https://dictionaryapi.dev/
+
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiURL).then(SearchResponse);
   }
 
   function KeywordChange(event) {
